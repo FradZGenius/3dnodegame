91379efec5775b9b0bfea45e615f2a9f0210ffd6
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2020-12-27 22:55:56 -0500

    added player movement and control.

--- client/js/script.js
+++ client/js/script.js
@@ -4,8 +4,6 @@ var mouse = new THREE.Vector2();
 
 var socket = io();
 
-var playerBox = new THREE.Box3();
-
 var playerVel = new THREE.Vector3();
 
 var ground;
@@ -13,6 +11,11 @@ var groundBox = new THREE.Box3();
 
 let testV = new THREE.Vector3(10,5,5);
 let testAxis = new THREE.Vector3(.707,.707,0);
+
+var clock = new THREE.Clock();
+
+var gravity = 100
+var speed = 100;
 
 function rad(degrees)
 {
@@ -42,6 +45,25 @@ var objs = [];
 var bboxTest;
 var controls;
 var testBox2;
+
+var keysDown = {};
+
+var sprinting = false;
+
+onKeydown = function(evnt){
+	keysDown[evnt.key.toLowerCase()] = true;
+	if(evnt.key === 'Shift'){
+		sprinting = true;
+	}
+}
+
+onKeyUp = function(evnt){
+	delete keysDown[evnt.key.toLowerCase()];
+	if(evnt.key === 'Shift'){
+		sprinting = false;
+	}
+}
+
 initScene = function() {
 	raycaster = new THREE.Raycaster();
 	
@@ -56,7 +78,7 @@ initScene = function() {
 	scene = new THREE.Scene;
 	
 	camera = new THREE.PerspectiveCamera(
-			45,
+			90,
 			window.innerWidth / window.innerHeight,
 			1,
 			1000
@@ -81,29 +103,27 @@ initScene = function() {
 
 	//socket.emit('debug',bboxTest.depth)
 
-	playerBox.setFromObject(player);
 
-	//scene.add( playerBox );
 	ground = new THREE.Mesh(
 			new THREE.BoxBufferGeometry(500,5,50),
-			new THREE.MeshBasicMaterial({ color: 0xffffff })
+			new THREE.MeshToonMaterial({ color: 0xffffff })
 	);
 	ground.name = "bruh";
 	scene.add(ground);
 
 	groundBox.setFromObject(ground);
 	ground.position.set(0,0,0)
-	ground.rotation.set(rad(180),0,rad(-25))
+	ground.rotation.set(rad(180),0,0)
 	
 	
 	testBox2 = new THREE.Mesh(
-		new THREE.BoxBufferGeometry(10,50,50),
+		new THREE.BoxBufferGeometry(10,10,10),
 		new THREE.MeshToonMaterial({color: 0xff00ff})
 	)
 
 	scene.add(testBox2);
-	testBox2.position.set(-100,-50,0)
-	testBox2.rotation.set(0,0,rad(90))
+	testBox2.position.set(-100,-10,0)
+	testBox2.rotation.set(0,0,rad(45))
 	let light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 	scene.add(light)
 	//console.log()
@@ -111,8 +131,6 @@ initScene = function() {
     //scene.add(groundBox);
 
     requestAnimationFrame( render );
-    const helper = new THREE.Box3Helper( playerBox, 0xffff00 );
-		scene.add( helper );
 	objs.push(ground, testBox2)
 };
 
@@ -122,30 +140,51 @@ initScene = function() {
 //take other 2 vectors that are not equal
 //to the normal of the face to 
 
+document.addEventListener('keydown',onKeydown);
+document.addEventListener('keyup',onKeyUp);
+
+
 render = function() {
 	//controls.lock();
 	//camera.lookAt( player.position );
-	let camLook = new THREE.Vector3(camera.matrix.elements[8],camera.matrix.elements[9],camera.matrix.elements[10]);
+	let delta = clock.getDelta();
+	let camLook = new THREE.Vector3(camera.matrix.elements[8],0,camera.matrix.elements[10]).normalize();
+	let camRight = new THREE.Vector3(camera.matrix.elements[0],camera.matrix.elements[1],camera.matrix.elements[2]);
+
 	raycaster.setFromCamera(mouse,camera);
-	if(raycaster.intersectObjects(scene.children)[0]){
-		//socket.emit('debug',raycaster.intersectObjects(scene.children))
-	}
+
 	renderer.render( scene, camera); // render the scene
 	requestAnimationFrame( render );
 
-	playerVel.add(new THREE.Vector3(0,-.0005,0))
+	let velAdd = new THREE.Vector3();
+	velAdd.add(new THREE.Vector3(0,-gravity,0))
+	
+	if(keysDown['w']){
+		let mult = -speed;
+		if(sprinting){
+			mult*=5;
+		}
+		velAdd.add(camLook.clone().multiplyScalar(mult));
+		console.log(keysDown)
+	}
+	if(keysDown['s']){
+		velAdd.add(camLook.clone().multiplyScalar(speed));
+	}
+	if(keysDown['a']){
+		velAdd.add(camRight.clone().multiplyScalar(-speed));
+	}
+	if(keysDown['d']){
+		velAdd.add(camRight.clone().multiplyScalar(speed));
+	}
 
-	let prevMin = playerBox.min.clone();
+	velAdd.multiplyScalar(delta);
+	playerVel.sub(new THREE.Vector3(playerVel.x, 0 ,playerVel.z).multiplyScalar(delta*7));
+	playerVel.add(velAdd)
+
 	bboxTest.updateValues();
 
-
-	playerBox.min.add(playerVel);
-
-	let prevMax = playerBox.max.clone();
-	playerBox.max.add(playerVel);
-
 	let prevPos = bboxTest.position;
-	bboxTest.position.add(playerVel);
+	bboxTest.position.add(playerVel.clone().multiplyScalar(delta));
 	bboxTest.intersectsBox(ground);
 	//ground.
 	objs.forEach((object)=>{
@@ -154,13 +193,18 @@ render = function() {
 		//}
 		//console.log(object)
 		if(bboxTest.intersectsBox(object)){
-			let testCaster = new THREE.Raycaster();
+			//let vA = new THREE.Vector3();
+			//let vB = new THREE.Vector3();
+			//let vC = new THREE.Vector3();
+			//let vD = new THREE.Vector3();
+
+
 			//to - from
 			let gbCenter = object.position.clone();
 
-			testCaster.set(player.position, (gbCenter.sub(player.position).normalize()));
+			raycaster.set(player.position, (gbCenter.sub(player.position).normalize()));
 			//console.log(testCaster.intersectObject(ground)[0].face.normal);
-			let intersect = testCaster.intersectObject(object)[0];
+			let intersect = raycaster.intersectObject(object)[0];
 			let face = intersect.face;
 			//console.log(intersect)
 
@@ -168,20 +212,18 @@ render = function() {
 			faceNorm.transformDirection(object.matrixWorld);
 			let proj = playerVel.clone().projectOnPlane(faceNorm);
 
+
+
 			//console.log(face);
 			playerVel.copy(proj);
 			
 		}
 		
 	});
-	playerBox.min.copy(prevMin.add(playerVel));
-	playerBox.max.copy(prevMax.add(playerVel));
 
-	bboxTest.position.copy(prevPos.add(playerVel));
+	bboxTest.position.copy(prevPos.add(playerVel.clone().multiplyScalar(delta)));
 
-	let b = new THREE.Vector3();
-	playerBox.getCenter(b);
-	player.position.copy(b);
+	player.position.copy(bboxTest.position);
 	camera.position.copy(player.position);
 };
 
