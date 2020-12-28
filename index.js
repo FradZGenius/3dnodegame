7639e2719a6712ac1ef95426bf83cffe7858e037
henry-lang <henry.langmack@gmail.com>
2020-12-27 23:08:52 -0500

    removes big brain
    w

--- index.js
+++ index.js
@@ -23,34 +23,3 @@ io.on('connection', (socket)=>{
 
 
 console.log('un momento bruh')
-
-//un momento bruh
-
-//|Proj ( T )| > |Proj ( WA*Ax )| + |Proj ( HA*Ay )| + |Proj( DA*Az )|
-//+ |Proj ( WB*Bx )| + |Proj( HB*By )| + |Proj( DB*Bz )|
-
-//15 total cases, L represents the axis
-/*
-L = Ax
-L = Ay
-L = Az
-L = Bx
-L = By
-L = Bz
-L = Ax X Bx
-L = Ax X By
-L = Ax X Bz
-L = Ay X Bx
-L = Ay X By
-L = Ay X Bz
-L = Az X Bx
-L = Az X By
-L = Az X Bz
-
-The separating plane spans the axes, the axis is 
-perpendicular to the plane, so you can just cross
-the two axes to get the separating axis.
-
-B must be a unit vector in order for the dot product
-to be equal to the magnitude projection
-*/
